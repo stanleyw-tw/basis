f22f1fde82664cf536ceadeba116e7a2763aab91
Stanley Wu <chengwu.tw@gmail.com>
2019-03-18 01:40:18 +0800

    implement running median

--- examples/running-median.cpp
+++ examples/running-median.cpp
@@ -1,10 +1,21 @@
 #include <cstdint>
+#include <iostream>
 #include <memory>
+#include <queue>
 #include <vector>
 
+#include "helpers.hpp"
 #include "running-median.hpp"
 
 namespace examples {
+
+template <typename T> void print_queue(T &q) {
+  while (!q.empty()) {
+    std::cout << q.top() << " ";
+    q.pop();
+  }
+  std::cout << '\n';
+}
 
 /**
  * @brief compute the median of the input vector a as follows:
@@ -19,9 +30,55 @@ namespace examples {
  */
 void runningMedian(const std::vector<int32_t> &a,
                    std::shared_ptr<std::vector<double>> ans) {
-  /*
-   * Write your code here.
-   */
+
+  if (a.empty()) {
+    return;
+  }
+
+  double median;
+  int32_t value;
+  uint64_t input_size = a.size();
+  std::priority_queue<int32_t, std::vector<int32_t>, std::less<int32_t>> max;
+  std::priority_queue<int32_t, std::vector<int32_t>, std::greater<int32_t>> min;
+
+  // for each element
+  for (uint64_t i = 0; i < input_size; i++) {
+    value = a.at(i);
+    if (i == 0) {
+      min.push(value);
+      median = static_cast<double>(value);
+      ans->push_back(median);
+    } else {
+      if (value > min.top()) {
+        min.push(value);
+      } else {
+        max.push(value);
+      }
+      // re-balance
+      if (max.size() > min.size()) {
+        min.push(max.top());
+        max.pop();
+      } else if (min.size() > max.size()) {
+        max.push(min.top());
+        min.pop();
+      }
+      // calculate running median
+      if ((max.size() + min.size()) % 2 == 0) {
+        median = static_cast<double>((max.top() + min.top())) / 2;
+        ans->push_back(median);
+      } else {
+        double max_v = (double)max.top();
+        double min_v = (double)min.top();
+        median = (max.size() > min.size()) ? max_v : min_v;
+        ans->push_back(median);
+      }
+
+    } // if first
+  }   // for loop
+  // for debug
+  // print_queue(max);
+  // print_queue(min);
+  // printVector(ans->begin(), ans->end());
 }
 
-}  // namespace examples
+} // namespace examples
