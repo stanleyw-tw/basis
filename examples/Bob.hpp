6ebbdd52db134a6a815879b2e2691fab8e6dfbbb
Stanley Wu <chengwu.tw@gmail.com>
2019-03-18 15:10:18 +0800

    fix memory leak in class Bob and Alice

--- examples/Bob.hpp
+++ examples/Bob.hpp
@@ -4,13 +4,14 @@
  * leaks, eventually.
  *
  * @file Bob.hpp
- * @author Fabian Franzelin
- * @date 2018-04-17
+ * @author Fabian Franzelin, Stanley Wu
+ * @date 2019-03-17
  */
 
 #pragma once
 
 #include <iostream>
+#include <memory>
 #include <vector>
 
 #include "helpers.hpp"
@@ -18,50 +19,58 @@
 namespace examples {
 
 class Bob {
- public:
-  Bob() { bobId = new std::vector<int>(1, 0); }
-  explicit Bob(int pBobId) { bobId = new std::vector<int>(1, pBobId); }
-  virtual ~Bob() { delete bobId; }
+public:
+  Bob() {
+    bobId = std::shared_ptr<std::vector<int>>(new std::vector<int>(1, 0));
+  }
+  explicit Bob(int pBobId) {
+    bobId = std::shared_ptr<std::vector<int>>(new std::vector<int>(1, pBobId));
+  }
+  virtual ~Bob() {
+    // delete bobId;
+  }
 
-  std::vector<int>* getIds() { return bobId; }
+  std::shared_ptr<std::vector<int>> getIds() { return bobId; }
 
- protected:
-  std::vector<int>* bobId;
+protected:
+  std::shared_ptr<std::vector<int>> bobId;
 };
 
 class Alice : public Bob {
- public:
+public:
   explicit Alice(int pAliceId, int pBobId) {
-    this->bobId = new std::vector<int>(1, pBobId);
-    aliceId = new std::vector<int>(1, pAliceId);
+    this->bobId =
+        std::shared_ptr<std::vector<int>>(new std::vector<int>(1, pBobId));
+    aliceId =
+        std::shared_ptr<std::vector<int>>(new std::vector<int>(1, pAliceId));
   }
-  virtual ~Alice() { delete aliceId; }
+  virtual ~Alice() {
+    // delete aliceId;
+  }
 
-  std::vector<int>* getIds() { return aliceId; }
+  std::shared_ptr<std::vector<int>> getIds() { return aliceId; }
 
- private:
-  std::vector<int>* aliceId;
+private:
+  std::shared_ptr<std::vector<int>> aliceId;
 };
 
 inline void checkBob() {
-  Bob* bob = new Bob(1);
+  Bob *bob = new Bob(1);
   std::cout << "Bob  : ";
   printVector(bob->getIds()->begin(), bob->getIds()->end());
 
-  Bob* alice = new Alice(2, (*bob->getIds())[0]);
+  Bob *alice = new Alice(2, (*bob->getIds())[0]);
   std::cout << "Alice: ";
   printVector(alice->getIds()->begin(), alice->getIds()->end());
 
   Bob bobby = *bob;
   std::cout << "Bobby: ";
   printVector(bobby.getIds()->begin(), bobby.getIds()->end());
-
   delete bob;
-
   std::cout << "Bobby: ";
   printVector(bobby.getIds()->begin(), bobby.getIds()->end());
 
   delete alice;
 }
 
-}  // namespace examples
+} // namespace examples
